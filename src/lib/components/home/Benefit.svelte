edd8782a0ac5b9edafa09fb0fa1c756273f1cf6a
Lucian Ivanov <ioniva@icloud.com>
2022-12-29 19:58:06 +0200

    Add benefits section

---
+++ src/lib/components/home/Benefit.svelte
@@ -0,0 +1,17 @@
+<section class="container mx-auto text-center">
+    <h2 class="text-5xl">Las ventajas de un préstamo <br> no bancario</h2>
+    <div class="flex mx-auto gap-60 mt-10">
+        <div class="flex flex-col items-center w-full">
+            <img src="src/lib/img/icons/24ore.png" alt="availability icon">
+            <p class="mt-6">Dinero en 48 horas con el servicio auxiliar y opcional de gestión de préstamo de vivienda</p>
+        </div>
+        <div class="flex flex-col items-center w-full">
+            <img src="src/lib/img/icons/ratefixe.png" alt="availability icon">
+            <p class="mt-6">Cuotas fijas semanales o mensuales según el préstamo elegido</p>
+        </div>
+        <div class="flex flex-col items-center w-full">
+            <img src="src/lib/img/icons/faragarantii.png" alt="availability icon">
+            <p class="mt-6">Sin garantías ni avales</p>
+        </div>
+    </div>
+</section>
