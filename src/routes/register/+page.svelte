eb4aa29b9938e719b0fe23407653bdddcca2b334
Lucian Ivanov <ioniva@icloud.com>
2022-12-31 20:53:18 +0200

    Add additional data component to register page

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -2,6 +2,7 @@
 	import { ProgressBar, Step, Stepper } from "@skeletonlabs/skeleton";
     import { writable } from "svelte/store";
 	import AccountData from "../../lib/components/register/AccountData.svelte";
+	import AdditionalData from "../../lib/components/register/AdditionalData.svelte";
 	import PersonalData from "../../lib/components/register/PersonalData.svelte";
     const active = writable(0);
     const onComplete = () => {
@@ -11,7 +12,7 @@
 
 <div class="container mx-auto">
     <h1 class="my-8">Register page</h1>
-    <Stepper {active} length={2} on:complete={onComplete}>
+    <Stepper {active} length={3} on:complete={onComplete}>
         <Step index={0}>
             <svelte:fragment slot="header">
                 <span class="text-3xl font-bold">Datos de tu cuenta</span>
@@ -23,6 +24,12 @@
                 <span class="text-3xl font-bold">Datos personales</span>
             </svelte:fragment>
             <PersonalData></PersonalData>
+        </Step>
+        <Step index={2}>
+            <svelte:fragment slot="header">
+                <span class="text-3xl font-bold">Datos adicionales</span>
+            </svelte:fragment>
+            <AdditionalData></AdditionalData>
         </Step>
     </Stepper>
 </div>
