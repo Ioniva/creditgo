ad826d77cc68201bc1133a56aaed2866419c634e
Lucian Ivanov <ioniva@icloud.com>
2023-01-16 08:28:30 +0200

    Remove temporal path

--- src/routes/temporal/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import RoundRangeSliderThree from "../../lib/components/RoundRangeSliderThree.svelte";
-
-
-	import RoundRangeSliderTwo from "../../lib/components/RoundRangeSliderTwo.svelte";
-
-</script>
-
-<RoundRangeSliderThree></RoundRangeSliderThree>
