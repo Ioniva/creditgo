63e5476fc9e1d4788329cc53f3f3c151f2f68ad7
Ioniva <ioniva@icloud.com>
2023-03-18 10:19:23 +0200

    Modify token prefix from "Bearer" to "bearer"

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,7 +4,7 @@ import {redirect} from '@sveltejs/kit';
 export const handle = async ({event, resolve}) => {
     const authCookie = event.cookies.get('AuthorizationToken');
     if (authCookie) {
-        const token = authCookie.replace('Bearer ', '');
+        const token = authCookie.replace('bearer ', '');
         try {
             const {id, email} = jwt.verify(token, import.meta.env.VITE_JWT_SECRET);
             const payload = {id, email};
