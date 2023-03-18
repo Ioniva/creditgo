544e6541d8e5a2f311a5c9de7d4d62c14beeca89
Ioniva <ioniva@icloud.com>
2023-03-18 10:20:10 +0200

    Remove dashboard route

--- src/routes/user/dashboard/+page.server.js
+++
@@ -1,7 +0,0 @@
-import {redirect} from '@sveltejs/kit';
-
-export const load = async (event) => {
-    const user = event.locals.user;
-    if (!user) throw redirect(307, "/login");
-    return user;
-}
