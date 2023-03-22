9e78a3cfc4853f6b394682e762d2fc455ea49f17
Ioniva <ioniva@icloud.com>
2023-03-22 19:59:09 +0200

    Add show solicitation user details into SelectReasonModal

--- src/lib/components/modals/SelectReasonModal.svelte
+++ src/lib/components/modals/SelectReasonModal.svelte
@@ -10,10 +10,19 @@
 
 	const selectedReason = writable('none');
 	let valorations = [];
+	let client = {};
 
 	onMount(async () => {
 		valorations = await getAllValorations();
+		client = await getSolicitationByUUID(uuid);
+		console.log(client);
 	});
+
+	const getSolicitationByUUID = async () => {
+		const url = `http://localhost:4000/api/v1/solicitations/details/${uuid}`;
+		const response = await FetchUtil.get(url);
+		return await response.solicitation.users[0];
+	};
 
 	const getAllValorations = async () => {
 		const url = 'http://localhost:4000/api/v1/rejection-reasons';
@@ -49,29 +58,45 @@
 		<div class="mt-2 flex flex-wrap gap-4">
 			<p class="flex-[1_0_auto%]">
 				<span class="font-bold">Nombre y apellidos: </span>
-				{parent.name}
-				{parent.surname}
-			</p>
-			<p class="flex-[3_0_auto%]">
-				<span class="font-bold">Edad: </span>
-				{parent.age}
+				{client.name}
+				{client.surname}
 			</p>
 			<p class="flex-[1_0_auto%]">
 				<span class="font-bold">Género: </span>
-				{parent.gendre}
+				{client.genre}
 			</p>
 			<p class="flex-[2_0_auto%]">
 				<span class="font-bold">Cédula: </span>
-				{parent.cedula}
+				{client.cedula}
 			</p>
 			<p class="flex-[1_0_auto%]">
 				<span class="font-bold">F.Exp: </span>
-				{parent.dispatchDate}
+				{client.dispatchDate}
 			</p>
 			<p class="flex-[1_0_auto%]">
 				<span class="font-bold">Lugar Exp: </span>
-				{parent.dispatchPlace}
+				{client.dispatchPlace}
 			</p>
+		</div>
+	</div>
+	<div class="h-1/2">
+		<div class="h-1/2 border-blue-700 mt-6">
+			<p class="unstyled text-2xl border-b-2 border-blue-700">Datos adicionales</p>
+			<div class="mt-2 flex flex-wrap gap-4">
+				<p class="flex-[3_0_auto%]">
+					<span class="font-bold">Ingreso neto mensual: </span>
+					{client.financial ? client.financial.netMonthlyIncome : ''}
+				</p>
+				<p class="flex-[3_0_auto%]">
+					<span class="font-bold">Ingresos adicionales: </span>
+					{client.financial ? client.financial.additionalIncome : ''}
+				</p>
+				<p class="flex-[1_0_auto%]">
+					<span class="font-bold">Tipo de empleado: </span>
+					<!-- TODO: El employeeType no deberia de estar dentro de financial. no tiene sentido -->
+					{client.financial ? client.financial.employeeType.name : ''}
+				</p>
+			</div>
 		</div>
 	</div>
 	<div class="h-1/2">
